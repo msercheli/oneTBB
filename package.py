2f845330ce18b0dbf8d0cd29f490a1b65fa54f33
Marcelo Sercheli <msercheli@gmail.com>
2020-11-23 16:09:31 -0500

    REZ using onetbb_2021 branch since TBB started using CMkake in that branch

---
+++ package.py
@@ -0,0 +1,22 @@
+name = "tbb"
+
+version = "2021.1.0"
+
+authors = [
+    "Intel"
+]
+
+description = \
+    """
+    Intel Threading Blocks
+    """
+
+requires = [
+    "gcc",
+]
+
+def pre_build_commands():
+    pass
+
+def commands():
+    pass
